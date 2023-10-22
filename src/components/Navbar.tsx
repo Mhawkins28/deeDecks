d65d8314f2618d66df6d3c9bf83ab31538fe0d52
Megan Hawkins <megan.hawkins28@gmail.com>
2023-10-22 11:04:34 -0400

    edit nav style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-  <nav className="flex justify-between items-center px-8 py-3 font-bold border border-slate-300">
-    <Link href={'/'}>Home</Link>
-    <Link href={'/alphabet'}>Alphabet</Link>
-    <Link href={'/numbers'}>Numbers</Link>
-    <Link href={'/phrases'}>Phrases</Link>
-  </nav>
-)}
+    <nav className='flex justify-between items-center px-8 py-3 font-bold '>
+      <Link className='hover:border-b-2 border-indigo-200' href={"/"}>
+        Home
+      </Link>
+      <Link href={"/alphabet"}>Alphabet</Link>
+      <Link href={"/numbers"}>Numbers</Link>
+      <Link href={"/phrases"}>Phrases</Link>
+    </nav>
+  );
+}
